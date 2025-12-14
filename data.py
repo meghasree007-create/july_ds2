9e20394c5d5073b147108dac332002a441207987
megha <meghasree007@gmail.com>
2025-12-14 23:34:05 +0530

    all files

---
+++ data.py
@@ -0,0 +1,23 @@
+
+def sub(x,y):
+    return x-y
+
+
+def add(a,b):
+    
+      return a+b
+def add(a,b):
+    
+      return a+b
+def add(a,b):
+    
+      return a+b
+def add(a,b):
+    
+      return a+b
+def add(a,b):
+    
+      return a+b
+def add(a,b):
+    
+      return a+b
